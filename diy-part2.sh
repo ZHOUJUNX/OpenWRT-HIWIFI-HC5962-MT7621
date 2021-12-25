183e7d8b44f1d2b4080dbc056fbaa37632d6309c
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2021-12-25 21:23:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo '### Updates default IP gate ###'
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+echo '###  ###'
+
+echo '### Updates Theme Argon ###'
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+echo '###  ###'
+
+echo '### 添加 mt7621 支持 ###'
+wget -P target/linux/ramips/images/ https://github.com/BenjaminX/OpenWrt-HiWiFi-HC5962-MT7621/blob/aab568a09abaa05df2559477ba9a6d7a316946fd/mt7621.mk
+echo '###  ###'
