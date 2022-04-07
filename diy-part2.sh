f71e82c58b437e3df81ba0de510718067bcad252
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2022-04-07 20:46:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo '###  ###'
 
 echo '### Updates Theme Argon ###'
-rm -rf package/lean/luci-theme-argon
+rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
 echo '###  ###'
 
